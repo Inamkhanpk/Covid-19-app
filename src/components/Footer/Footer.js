80a7e7dfec0cc4806fa5e1d148caa398dc97f229
Inam Khan <inamk2111@gmail.com>
2020-07-09 05:51:01 +0000

    make app awesome

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = ()=>{
     return(
         <div className="text-center footer-bg">
-         <p>Made by Inam Kan</p>
+         {/* <p>Made by Inam Kan</p> */}
         </div>
     )
 }
